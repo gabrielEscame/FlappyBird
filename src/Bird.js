d78e43e4b1ac26fc8d22306a736d81f8a28415df
Gabriel Escame <gabriel.escame@gmail.com>
2020-12-28 00:17:36 -0300

    feat: bird keyframe animation

---
+++ src/Bird.js
@@ -0,0 +1,52 @@
+class Bird extends ViewElement {
+  constructor(ctx, img, spriteX, spriteY, width, height, posX, posY) {
+    super(ctx, img, spriteX, spriteY, width, height, posX, posY)
+    this.velocity = 0
+    this.gravity = 0.25
+    this.jumpForce = 4.75
+    this.currentFrame = 0
+    this.keyframes = [
+      {
+        spriteX, spriteY
+      },
+      {
+        spriteX, spriteY: 26
+      },
+      {
+        spriteX, spriteY: 52
+      }
+    ]
+  }
+  draw() {
+    const { spriteX, spriteY } = this.keyframes[this.currentFrame]
+    this.ctx.drawImage(
+      this.img,
+      spriteX, spriteY,
+      this.width, this.height,
+      this.posX, this.posY,
+      this.width, this.height
+    )
+  }
+  update() {
+    const keyframeUpdate = frames % 10 === 0
+    const birdPosition = this.posY + this.height
+    const floorPosition = view.height - 112
+
+    if (keyframeUpdate) {
+      this.currentFrame = frames % 3
+    }
+
+    if (birdPosition >= floorPosition) {
+      this.currentFrame = 1
+      return
+    }
+
+    this.velocity = this.velocity + this.gravity
+    this.posY = this.posY + this.velocity
+  }
+  jump() {
+    window.addEventListener("click", () => {
+      this.velocity = - this.jumpForce
+    })
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+let frames = 0
+
 const sprites = new Image()
 sprites.src =  "../public/sprites.png"
 
@@ -6,12 +8,17 @@ const ctx = view.getContext("2d")
 
 const buildings = new ViewElement(ctx, sprites, 110, 370, 552, 204, 0, view.height - 204)
 const floor = new ViewElement(ctx, sprites, 0, 610, 448, 112, 0, view.height - 112)
-const player = new ViewElement(ctx, sprites, 0, 0, 33, 24, 10, 50)
+const player = new Bird(ctx, sprites, 0, 0, 33, 24, 10, 50)
 
 const setUp = () => {
+  ctx.fillStyle = "#70c5ce"
+  ctx.fillRect(0, 0, view.width, view.height)
   buildings.draw()
   floor.draw()
+  player.update()
   player.draw()
+  player.jump()
+  frames += 1
 }
 
 const engine = () => {
